6546f88c802a79e40a16714da2f29d9136938bbe
Brandon Luo <brandon.y.luo@gmail.com>
2024-04-13 16:58:07 -0700

    update sequences

--- src/models/utils.py
+++ src/models/utils.py
@@ -16,3 +16,11 @@ def create_sequences(input_data, state_data, returns_data, lookback_window1, loo
         ys.append(y)
     return np.array(x1s), np.array(x2s), np.array(ys)
 
+def create_sequences_replication(input_data, returns_data, lookback_window, forward_window, interval):
+    xs, ys = [], []
+    for i in range(0, len(input_data)-lookback_window-forward_window+1, interval):
+        x = input_data[i:(i+lookback_window)]  # the prior n1 data
+        y = returns_data[(i+lookback_window):(i+lookback_window+forward_window)]  # need the next n2 returns
+        xs.append(x)
+        ys.append(y)
+    return np.array(xs), np.array(ys)
